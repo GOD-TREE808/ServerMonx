6b34dd90ac5b9212a45c786cba3c10c3e9e51bd6
GOD-TREE808 <disiyampho@gmail.com>
2025-11-07 22:26:28 +0200

    Add files via upload

---
+++ script (2).js
@@ -0,0 +1,12 @@
+document.getElementById("blank").addEventListener(
+  "click", function(){
+    window.location.href="page1.html";
+  });
+document.getElementById("blank2").addEventListener(
+  "click", function(){
+    window.location.href="page2.html";
+  })
+document.getElementById("blank3").addEventListener(
+  "click", function(){
+    window.location.href="page3.html";
+  })
